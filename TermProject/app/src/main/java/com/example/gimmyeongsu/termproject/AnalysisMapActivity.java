557eb5127e5a354c935f49c181d8cb9d959ad41f
Kim Myung Soo <msbmkim@gmail.com>
2016-12-01 15:03:43 +0900

    기본 레이아웃 구현
    화면전환가능.
    구글맵 연동.

--- TermProject/app/src/main/java/com/example/gimmyeongsu/termproject/AnalysisMapActivity.java
+++ TermProject/app/src/main/java/com/example/gimmyeongsu/termproject/AnalysisMapActivity.java
@@ -1,8 +1,17 @@
 package com.example.gimmyeongsu.termproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by gimmyeongsu on 2016. 11. 29..
@@ -10,9 +19,33 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AnalysisMapActivity extends AppCompatActivity {
 
+    Button button_time;
+    GoogleMap gmap;
+
+
+    LatLng location = new LatLng(37.559603, 126.982203);
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_map);
+
+        gmap = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapView)).getMap();
+        gmap.addMarker(new MarkerOptions().position(location).title("good").snippet("hello"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
+
+
+        button_time = (Button)findViewById(R.id.button_Time);
+        button_time.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent time = new Intent(getApplicationContext(), AnalysisTimeActivity.class);
+                startActivity(time);
+            }
+        });
     }
+
+
 }
--- TermProject/app/src/main/java/com/example/gimmyeongsu/termproject/AnalysisTimeActivity.java
+++ TermProject/app/src/main/java/com/example/gimmyeongsu/termproject/AnalysisTimeActivity.java
@@ -1,8 +1,11 @@
 package com.example.gimmyeongsu.termproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by gimmyeongsu on 2016. 11. 29..
@@ -10,9 +13,24 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AnalysisTimeActivity extends AppCompatActivity {
 
+
+    Button button_map;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analysis_time);
+
+        button_map=(Button)findViewById(R.id.button_Map);
+        button_map.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                finish();
+                Intent map = new Intent(getApplicationContext(), AnalysisMapActivity.class);
+                startActivity(map);
+            }
+        });
     }
+
 }
